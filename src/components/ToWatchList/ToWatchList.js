52f48728d21393b6a0997eb020867cd60c536364
Emil Zainagabdinov <screw192@gmail.com>
2021-01-15 14:12:20 +0600

    Added ToWatchList.js. movieList returns array of elements with movies.

---
+++ src/components/ToWatchList/ToWatchList.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import ToWatchListItem from "../ToWatchListItem/ToWatchListItem";
+
+class ToWatchList extends Component {
+  render() {
+    const movieList = this.props.list.map(item => {
+      return (
+          <ToWatchListItem
+              key={item.id}
+              movie={item.movie}
+              id={item.id}
+              edit={this.props.edit}
+              remove={this.props.remove}
+          />
+      );
+    });
+
+    return (
+        <div className="ToWatchList">
+          {movieList}
+        </div>
+    );
+  }
+}
+
+export default ToWatchList;
