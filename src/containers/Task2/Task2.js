e4a234c9ab9cd297b1ad9f39f7340c64ea928e80
Emil Zainagabdinov <screw192@gmail.com>
2021-01-15 14:32:40 +0600

    Created Task2.js. Added state with empty array for jokes. Added class for parent element.

---
+++ src/containers/Task2/Task2.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+const Task2 = () => {
+  const [jokes, setJokes] = useState([]);
+
+  return (
+      <div className="TaskBlock">
+
+      </div>
+  );
+};
+
+export default Task2;
