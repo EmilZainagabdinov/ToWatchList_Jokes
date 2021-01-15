b66242f12debb2d55eba5fc1b40d8be9e9abb45d
Emil Zainagabdinov <screw192@gmail.com>
2021-01-15 16:45:16 +0600

    Imported Joke component. Added url for jokes request. Added request function.

--- src/containers/Task2/Task2.js
+++ src/containers/Task2/Task2.js
@@ -1,11 +1,27 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import Joke from "../../components/Joke/Joke";
 
 const Task2 = () => {
   const [jokes, setJokes] = useState([]);
 
+  const url = "https://api.chucknorris.io/jokes/random";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url);
+
+      if (response.ok) {
+        const joke = await response.json();
+        setJokes(joke);
+      }
+    }
+
+    fetchData().catch(console.error);
+  }, []);
+
   return (
       <div className="TaskBlock">
-
+        <Joke joke={jokes} />
       </div>
   );
 };
